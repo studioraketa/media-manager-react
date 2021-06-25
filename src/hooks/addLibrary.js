6144627c4e27e538ebc8b8295ce98fa3461e7643
Vassil Christov <vassil.christov@gmail.com>
2021-06-26 00:33:00 +0300

    addLibrary export fetch function to handle events

--- src/hooks/addLibrary.js
+++ src/hooks/addLibrary.js
@@ -2,51 +2,50 @@ import { useEffect, useState } from "react";
 
 import { url, key } from "./configuration";
 
-const addLibrary = (reqBody) => {
-  const [state, setState] = useState({
+const addLibrary = () => {
+  const [data, setData] = useState({
     loading: false,
     error: null,
     data: [],
   });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setState((prevState) => ({
+  const fetchLibraries = async () => {
+    setData((prevState) => ({
+      ...prevState,
+      loading: true,
+    }));
+
+    const demoBody = { library: { uid: "demo-uid", name: "DemoLibrary" } };
+
+    const response = await fetch(url + "/libraries", {
+      method: "POST",
+      headers: {
+        Authorization: key,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(demoBody),
+    });
+
+    const data = await response.json();
+    if (data.message) {
+      setData((prevState) => ({
         ...prevState,
-        loading: true,
+        error: data.message,
       }));
+    } else if (data) {
+      setData((prevState) => ({
+        loading: false,
+        error: null,
+        data: data,
+      }));
+    }
+  };
 
-      const demoBody = { library: { uid: "demo-uid", name: "DemoLibrary" } };
-
-      const response = await fetch(url + "/libraries", {
-        method: "POST",
-        headers: {
-          Authorization: key,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(demoBody),
-      });
-
-      const data = await response.json();
-      if (data.message) {
-        setState((prevState) => ({
-          ...prevState,
-          error: data.message,
-        }));
-      } else if (data) {
-        setState((prevState) => ({
-          loading: false,
-          error: null,
-          data: data,
-        }));
-      }
-    };
-
-    fetchData();
+  useEffect(() => {
+    fetchLibraries();
   }, []);
-  console.log(state);
 
-  return state;
+  return [fetchLibraries, data];
 };
 
 export default addLibrary;
